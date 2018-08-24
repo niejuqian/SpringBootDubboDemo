dd7cfb693025945eb54a4835fca3855a938dc3d3
6213 <6213@cunpiao.com>
2018-08-24 16:35:34 +0800

    优化

--- spring-boot-consumer/src/main/java/com/springboot/dubbo/demo/consumer/ConsumerApplication.java
+++ spring-boot-consumer/src/main/java/com/springboot/dubbo/demo/consumer/ConsumerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 消费者服务器启动入口及配置
@@ -17,6 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.springboot.dubbo.demo")
 @EntityScan("com.springboot.dubbo.demo.**.entity")
 @EnableJpaRepositories("com.springboot.dubbo.demo.**.jpa")
+@EnableTransactionManagement // 可以不用显示的增加此注解，spring boot在引入相应的orm时，已经默认开启事务支持了
 public class ConsumerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder().sources(ConsumerApplication.class,ConsumerDubboConfig.class,ConsumerWebConfig.class).run(args);
