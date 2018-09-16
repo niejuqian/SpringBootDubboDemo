acb55012d18bf217b64a52efc540bb4c8a02fc65
laonie <515867508@qq.com>
2018-09-16 15:47:57 +0800

    优化配置

--- spring-boot-consumer/src/main/java/com/springboot/dubbo/demo/consumer/ConsumerWebConfig.java
+++ spring-boot-consumer/src/main/java/com/springboot/dubbo/demo/consumer/ConsumerWebConfig.java
@@ -1,5 +1,8 @@
 package com.springboot.dubbo.demo.consumer;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.springboot.dubbo.demo.common.web.AppExceptionHandlerResolver;
 import com.springboot.dubbo.demo.common.web.LogParamInterceptor;
 import com.springboot.dubbo.demo.common.web.RequestBodyReadFilter;
@@ -7,6 +10,8 @@ import com.springboot.dubbo.demo.common.web.ResponseBodyWrapperHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -14,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -55,6 +61,25 @@ public class ConsumerWebConfig extends WebMvcConfigurerAdapter{
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
         super.extendHandlerExceptionResolvers(exceptionResolvers);
         exceptionResolvers.add(appExceptionHandlerResolver);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        //1.需要先定义一个 convert 转换消息的对象;
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        List<MediaType> supportedMediaTypes = new ArrayList<MediaType>();
+        supportedMediaTypes.add(MediaType.parseMediaType("text/html;charset=UTF-8"));
+        supportedMediaTypes.add(MediaType.parseMediaType("application/json"));
+        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
+        //2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        // 忽略对象属性中的null值
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //3、在convert中添加配置信息.
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        //4、将convert添加到converters当中.
+        converters.add(fastConverter);
     }
 
     @Bean
